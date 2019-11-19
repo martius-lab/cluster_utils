650c47aa025b9f107ed0a0067484beedf2d64d37
Michal Rolinek <michalrolinek@gmail.com>
2019-11-19 17:59:44 +0100

    execute only as main

--- cluster/optimize_hyperparams.py
+++ cluster/optimize_hyperparams.py
@@ -34,55 +34,56 @@ def get_git_url():
 
     return None
 
+if __name__ == '__main__':
 
-params = update_params_from_cmdline(verbose=False)
+    params = update_params_from_cmdline(verbose=False)
 
-json_full_name = os.path.abspath(sys.argv[1])
-init_plotting()
+    json_full_name = os.path.abspath(sys.argv[1])
+    init_plotting()
 
-opt_procedure_name = params.optimization_procedure_name
+    opt_procedure_name = params.optimization_procedure_name
 
-home = str(Path.home())
-main_path = mkdtemp(suffix=opt_procedure_name + "-" + "project")
-results_path = os.path.join(home, params.results_dir, opt_procedure_name)
-jobs_path = mkdtemp(suffix=opt_procedure_name + "-" + "jobs")
-
-
-given_url = params.git_params.get("url")
-if not given_url:
-    auto_url = get_git_url()
-    if not auto_url:
-        raise git.exc.InvalidGitRepositoryError("No git repository given in json file or auto-detected")
-
-    git_params = dict(url=auto_url, local_path=main_path, **params.git_params)
-
-else:
-    git_params = dict(local_path=main_path, **params.git_params)
-
-base_paths_and_files = dict(
-    script_to_run=os.path.join(main_path, params.script_relative_path),
-    result_dir=results_path,
-    jobs_dir=jobs_path,
-    **params.environment_setup
-)
-
-distribution_list = [get_distribution(**item) for item in params.optimized_params]
+    home = str(Path.home())
+    main_path = mkdtemp(suffix=opt_procedure_name + "-" + "project")
+    results_path = os.path.join(home, params.results_dir, opt_procedure_name)
+    jobs_path = mkdtemp(suffix=opt_procedure_name + "-" + "jobs")
 
 
-# noinspection PyUnusedLocal
-def find_json(df, path_to_results, filename_generator):
-    return json_full_name
+    given_url = params.git_params.get("url")
+    if not given_url:
+        auto_url = get_git_url()
+        if not auto_url:
+            raise git.exc.InvalidGitRepositoryError("No git repository given in json file or auto-detected")
+
+        git_params = dict(url=auto_url, local_path=main_path, **params.git_params)
+
+    else:
+        git_params = dict(local_path=main_path, **params.git_params)
+
+    base_paths_and_files = dict(
+        script_to_run=os.path.join(main_path, params.script_relative_path),
+        result_dir=results_path,
+        jobs_dir=jobs_path,
+        **params.environment_setup
+    )
+
+    distribution_list = [get_distribution(**item) for item in params.optimized_params]
 
 
-json_hook = SectionFromJsonHook(section_title="Optimization setting script", section_generator=find_json)
+    # noinspection PyUnusedLocal
+    def find_json(df, path_to_results, filename_generator):
+        return json_full_name
 
-hyperparameter_optimization(
-    base_paths_and_files=base_paths_and_files,
-    submission_requirements=params.cluster_requirements,
-    distribution_list=distribution_list,
-    other_params=params.fixed_params,
-    git_params=git_params,
-    report_hooks=[json_hook],
-    num_best_jobs_whose_data_is_kept=params.num_best_jobs_whose_data_is_kept,
-    **params.optimization_setting,
-)
+
+    json_hook = SectionFromJsonHook(section_title="Optimization setting script", section_generator=find_json)
+
+    hyperparameter_optimization(
+        base_paths_and_files=base_paths_and_files,
+        submission_requirements=params.cluster_requirements,
+        distribution_list=distribution_list,
+        other_params=params.fixed_params,
+        git_params=git_params,
+        report_hooks=[json_hook],
+        num_best_jobs_whose_data_is_kept=params.num_best_jobs_whose_data_is_kept,
+        **params.optimization_setting,
+    )
