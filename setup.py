7e6f89d12f42db5f7f3186af0a39aa446884c4df
Michal Rolinek <michalrolinek@gmail.com>
2020-01-06 18:59:12 +0100

    Added colorama dependency

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(name='cluster',
       license='MIT',
       packages=['cluster'],
       install_requires=['gitpython>=3.0.5', 'pathlib2', 'seaborn', 'pandas', 'matplotlib', 'sklearn', 'numpy',
-                        'nevergrad @ git+https://github.com/facebookresearch/nevergrad.git', 'tqdm',
+                        'nevergrad @ git+https://github.com/facebookresearch/nevergrad.git', 'tqdm', 'colorama',
                         'pyuv', 'cloudpickle'],
       zip_safe=False)
