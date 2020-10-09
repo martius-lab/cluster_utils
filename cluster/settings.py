38a4bbd0bc6290db6714cc6091e71c65371b2820
Marin Vlastelica <marin.vlastelica@tue.mpg.de>
2020-10-09 18:45:55 +0200

    nicer imports

--- cluster/settings.py
+++ cluster/settings.py
@@ -14,13 +14,11 @@ import pyuv
 import smart_settings
 from smart_settings.param_classes import recursive_objectify
 
-from .utils import update_recursive
-
 import cluster.submission_state as submission_state
 from .communication_server import MessageTypes
 from .constants import *
 from .optimizers import Metaoptimizer, NGOptimizer, GridSearchOptimizer
-from .utils import flatten_nested_string_dict, save_dict_as_one_line_csv
+from .utils import flatten_nested_string_dict, save_dict_as_one_line_csv, update_recursive
 
 def cluster_main(main_func, *decorator_args, **decorator_kwargs):
 
