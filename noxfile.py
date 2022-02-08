b41b8f4c2f79b992eb2cce1c2a2316f08db2b582
Maximilian Seitzer <maximilian.seitzer@tuebingen.mpg.de>
2022-02-08 17:27:16 +0100

    Setup for pre-commit and black

--- noxfile.py
+++ noxfile.py
@@ -2,15 +2,23 @@ import tempfile
 
 import nox
 
-LOCATIONS = ('cluster/', 'examples/', 'noxfile.py', 'setup.py')
+nox.options.sessions = ("lint", "tests")
+LOCATIONS = ('cluster/', 'examples/', 'tests/', 'noxfile.py', 'setup.py')
 
 
 @nox.session(python=['3.6'])
 def lint(session):
     session.install('flake8')
+    session.install('flake8-black')
     session.install('flake8-bugbear')
     session.install('flake8-isort')
     session.run('flake8', *LOCATIONS)
+
+
+@nox.session(python="3.6")
+def black(session):
+    session.install('black')
+    session.run("black", *LOCATIONS)
 
 
 @nox.session(python=['3.6'])
