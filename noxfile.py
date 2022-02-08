e571888f23dbecbecf987788f31566a17d98c647
Maximilian Seitzer <maximilian.seitzer@tuebingen.mpg.de>
2022-02-08 18:13:49 +0100

    Fix tooling settings

--- noxfile.py
+++ noxfile.py
@@ -3,28 +3,26 @@ import tempfile
 import nox
 
 nox.options.sessions = ("lint", "tests")
-LOCATIONS = ('cluster/', 'examples/', 'tests/', 'noxfile.py', 'setup.py')
+LOCATIONS = ("cluster/", "examples/", "tests/", "noxfile.py", "setup.py")
 
 
-@nox.session(python=['3.6'])
+@nox.session(python=["3.6"])
 def lint(session):
-    session.install('flake8')
-    session.install('flake8-black')
-    session.install('flake8-bugbear')
-    session.install('flake8-isort')
-    session.run('flake8', *LOCATIONS)
+    session.install("flake8")
+    session.install("flake8-bugbear")
+    session.install("flake8-isort")
+    session.run("flake8", *LOCATIONS)
 
 
 @nox.session(python="3.6")
 def black(session):
-    session.install('black')
+    session.install("black")
     session.run("black", *LOCATIONS)
 
 
-@nox.session(python=['3.6'])
+@nox.session(python=["3.6"])
 def tests(session):
-    session.install('.')
+    session.install(".")
 
     with tempfile.TemporaryDirectory() as test_dir:
-        session.run('bash', 'tests/run_integration_tests.sh', test_dir,
-                    external=True)
+        session.run("bash", "tests/run_integration_tests.sh", test_dir, external=True)
