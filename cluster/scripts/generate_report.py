8023d0eacb1fcfe449c810cf18e059079ae0bf9d
Felix Kloss <felix.widmaier@tuebingen.mpg.de>
2023-10-16 11:36:27 +0200

    Coloured logging output

--- cluster/scripts/generate_report.py
+++ cluster/scripts/generate_report.py
@@ -11,8 +11,44 @@ import pathlib
 import pickle
 import sys
 
+import colorama
+
 from cluster.constants import STATUS_PICKLE_FILE, SUBMISSION_HOOK_STATS_FILE
 from cluster.optimizers import Optimizer
+
+
+def configure_logging(verbose: bool) -> logging.Logger:
+    """Configure logger instance."""
+
+    class ColouredFormatter(logging.Formatter):
+        STYLES = {
+            "WARNING": colorama.Fore.YELLOW,
+            "INFO": colorama.Fore.BLUE,
+            "DEBUG": colorama.Fore.GREEN,
+            "CRITICAL": colorama.Fore.RED + colorama.Style.BRIGHT,
+            "ERROR": colorama.Fore.RED,
+        }
+
+        def __init__(self, *, fmt):
+            logging.Formatter.__init__(self, fmt=fmt)
+
+        def format(self, record: logging.LogRecord) -> str:  # noqa: A003
+            msg = super().format(record)
+            try:
+                return f"{self.STYLES[record.levelname]}{msg}{colorama.Style.RESET_ALL}"
+            except KeyError:
+                return msg
+
+    log_handler = logging.StreamHandler()
+    log_handler.setFormatter(
+        ColouredFormatter(fmt="[%(asctime)s] [%(name)s | %(levelname)s] %(message)s")
+    )
+
+    logger = logging.getLogger("generate_report")
+    logger.addHandler(log_handler)
+    logger.setLevel(logging.DEBUG if verbose else logging.INFO)
+
+    return logger
 
 
 def main() -> int:
@@ -41,25 +77,27 @@ def main() -> int:
     )
     args = parser.parse_args()
 
-    logging.basicConfig(
-        level=logging.DEBUG if args.verbose else logging.INFO,
-        format="[%(asctime)s] [%(name)s | %(levelname)s] %(message)s",
-    )
+    logger = configure_logging(args.verbose)
 
     if not args.results_dir.is_dir():
-        logging.fatal("'%s' does not exist or is not a directory", args.results_dir)
+        logger.fatal("'%s' does not exist or is not a directory", args.results_dir)
         return 1
 
     status_file = args.results_dir / STATUS_PICKLE_FILE
     if not status_file.exists():
-        logging.fatal("Status file '%s' does not exist", status_file)
+        logger.fatal("Status file '%s' does not exist", status_file)
         return 1
 
-    if not args.force and args.output.exists():
-        val = input(f"{args.output} already exists.  Overwrite it? [y/N]: ")
-        if val.lower() not in ["y", "yes"]:
-            print("Do not overwrite existing file.  Abort.")
-            return 1
+    if args.output.exists():
+        if args.force:
+            logger.warning(
+                f"{args.output} already exists but will be overwritten due to --force."
+            )
+        else:
+            val = input(f"{args.output} already exists.  Overwrite it? [y/N]: ")
+            if val.lower() not in ["y", "yes"]:
+                print("Do not overwrite existing file.  Abort.")
+                return 1
 
     with open(status_file, "rb") as f:
         optimizer: Optimizer = pickle.load(f)
@@ -71,7 +109,7 @@ def main() -> int:
         with open(submission_hook_stats_file, "rb") as f:
             submission_hook_stats = pickle.load(f)
     except Exception as e:
-        logging.error(
+        logger.error(
             "Failed to load submission hook stats from '%s': %s",
             submission_hook_stats_file,
             e,
@@ -79,10 +117,11 @@ def main() -> int:
         submission_hook_stats = {}
 
     if not isinstance(optimizer, Optimizer):
-        logging.warning("Object loaded from '%s' is not of type Optimizer", status_file)
+        logger.warning("Object loaded from '%s' is not of type Optimizer", status_file)
 
     # TODO: does this write anything to results_dir?
     optimizer.save_pdf_report(args.output, submission_hook_stats, args.results_dir)
+    logger.info("Saved report to %s", args.output)
 
     return 0
 
