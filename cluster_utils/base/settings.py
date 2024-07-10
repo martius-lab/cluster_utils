4622a42c307a437cab070fcccf4d1573eb831246
Felix Kloss <felix.kloss@uni-tuebingen.de>
2024-07-10 12:34:22 +0200

    Use SettingsError in check_reserved_params
    Since this is a settings-related failure, it seems more appropriate.
    
    Also use more generic Mapping instead of dict for type hint.

--- cluster_utils/base/settings.py
+++ cluster_utils/base/settings.py
@@ -1,3 +1,5 @@
+from typing import Any, Mapping
+
 from cluster_utils.base import constants
 
 
@@ -7,12 +9,12 @@ class SettingsError(Exception):
     ...
 
 
-def check_reserved_params(orig_dict: dict) -> None:
-    """Check if the given dict contains reserved keys.  If yes, raise ValueError."""
+def check_reserved_params(orig_dict: Mapping[str, Any]) -> None:
+    """Check if the given dict contains reserved keys.  If yes, raise SettingsError."""
     for key in orig_dict:
         if key in constants.RESERVED_PARAMS:
             msg = f"'{key}' is a reserved param name"
-            raise ValueError(msg)
+            raise SettingsError(msg)
 
 
 def add_cmd_line_params(base_dict, extra_flags):
