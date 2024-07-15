43af56cd949fa09a10b30569b763e6cd96f6e1fb
Felix Kloss <felix.kloss@uni-tuebingen.de>
2024-07-15 17:01:55 +0200

    refactor: Move packages to src/
    This allows setuptools to auto-detect the packages, so we don't need to
    list them all in pyproject.toml.

--- noxfile.py
+++ noxfile.py
@@ -7,8 +7,7 @@ import nox
 PYTHON_VERSIONS = ["3.8", "3.9", "3.10", "3.11", "3.12"]
 
 LOCATIONS = (
-    "cluster_utils/",
-    "cluster/",
+    "src/",
     "examples/",
     "tests/",
     "noxfile.py",
